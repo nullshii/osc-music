f316590e5ce59fc4c93afec53a392c6ca3caed6b
nullshii <null@>
2024-06-10 12:28:20 +0400

    Kinda fixed memory leak :)

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
+use media_session::MediaSession;
 use std::time::Duration;
 use vrc_osc::VrcOsc;
-use media_session::MediaSession;
 
-mod vrc_osc;
 mod media_session;
+mod vrc_osc;
 
 #[tokio::main]
 async fn main() {
-    let vrc = VrcOsc::new("127.0.0.1:9000".to_string()).expect("Can't create VrcOsc Object!");
+    tokio::spawn(check_music_update())
+        .await
+        .expect("Thread ERROR!!");
+}
 
+async fn check_music_update() {
+    let vrc = VrcOsc::new("127.0.0.1:9000".to_string()).expect("Can't create VrcOsc Object!");
     let mut last_title = String::new();
 
     loop {
-        if let Ok(s) = MediaSession::new().await {
-            let title = s.get_title();
+        tokio::time::sleep(Duration::from_millis(5_000)).await;
 
-            if last_title == title {
-                continue;
-            }
+        let (artist, title) = get_data().await;
 
-            let text = format!("{} - {}", s.get_artist(), title);
-
-            if let Err(e) = vrc.send_message(text) {
-                println!("{}", e)
-            }
-
-            last_title = title;
+        if title == "" || last_title == title {
+            continue;
         }
 
-        tokio::time::sleep(Duration::from_millis(2000)).await;
+        let text = format!("{} - {}", artist, title);
+        println!("{}", text);
+        
+        if let Err(e) = vrc.send_message(text) {
+            println!("{}", e)
+        }
+
+        tokio::time::sleep(Duration::from_millis(10_000)).await;
+
+        if let Err(e) = vrc.send_message(String::new()) {
+            println!("{}", e)
+        }
+
+        last_title = title;
     }
+}
+
+async fn get_data() -> (String, String) {
+    if let Ok(s) = MediaSession::new().await {
+        return (s.get_artist(), s.get_title());
+    }
+
+    return (String::new(), String::new());
 }
